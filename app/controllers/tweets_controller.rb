3b58189cb0f9f29742944508e8b318fbb3fcd8d9
ludndev <ludndev@gmail.com>
2021-07-29 19:31:00 +0100

    implement index of tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,31 +1,34 @@
 class TweetsController < ApplicationController
 
   def index
-
-  end
-
-  def create
-
+    @title = "List of Tweets"
+    @posts = Post.all
   end
 
   def new
-
+    @title = "title"
   end
 
   def edit
-
+    @title = "title"
   end
 
   def show
+    @title = "Tweet "
+  end
 
+  # ---
+
+  def create
+    @title = "title"
   end
 
   def update
-
+    @title = "title"
   end
 
   def destroy
-
+    @title = "title"
   end
 
 end
