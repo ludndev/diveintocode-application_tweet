760ac179b97d7e44ba20fc84505830b647089097
ludndev <ludndev@gmail.com>
2021-07-29 17:55:10 +0100

    enhance validatation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, allow_blank: false, allow_nil: false, length: {
+    minimum: 1,
+    maximum: 140,
+    too_short: "%{count} characters is too short. Enter at least 1 character.",
+    too_long: "%{count} characters is the maximum allowed"
+  }
 end
