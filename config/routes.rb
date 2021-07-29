5061a4f2a5a266207f9a2b24f4131aabf671098e
ludndev <ludndev@gmail.com>
2021-07-29 16:25:11 +0100

    add tweets index and root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'tweets#index'
+
+  get '/list', to: 'tweets#index', as: 'index'
+
 end
