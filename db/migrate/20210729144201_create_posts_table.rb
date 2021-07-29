085aa6ab274475c8780985fdc5258c4b88be74dd
ludndev <ludndev@gmail.com>
2021-07-29 16:02:19 +0100

    init Post model

--- db/migrate/20210729144201_create_posts_table.rb
+++
@@ -1,8 +0,0 @@
-class CreatePostsTable < ActiveRecord::Migration[6.1]
-  def change
-    create_table :posts_tables do |t|
-      t.string :content, null: false
-      t.timestamps
-    end
-  end
-end
